8257a0086a244fda5719191c940c151ef5509fa7
PhamTienLam <lampham251020000@gmail.com>
2022-03-01 14:27:57 +0700

    conmit dn thanh cong

--- baitestgit/baitestgit/Form1.Designer.cs
+++ baitestgit/baitestgit/Form1.Designer.cs
@@ -85,6 +85,7 @@ namespace baitestgit
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Form1
             // 
